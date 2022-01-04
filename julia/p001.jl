801483690c27ce6c7f0432e5410f9e77c3f65a15
eric <emtordo@gmail.com>
2022-01-04 12:11:19 -0500

    added problem descriptions

--- julia/p001.jl
+++ julia/p001.jl
@@ -1,10 +1,16 @@
 # Problem 1: Multiples of 3 or 5
 # https://projecteuler.net/problem=1
 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+# get 3, 5, 6 and 9. The sum of these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+N = 1000
 x = 0
-for n in 1:999
-    if mod(n, 3) == 0 || mod(n , 5) == 0
-        global x += n
+for i in 1:(N - 1)
+    if mod(i, 3) == 0 || mod(i , 5) == 0
+        global x += i
     end
 end
 
