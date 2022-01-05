f5b81c4d8233d843241a667269221273e5080e50
eric <emtordo@gmail.com>
2022-01-05 16:46:13 -0500

    Problem 5 solution

---
+++ julia/p005.jl
@@ -0,0 +1,15 @@
+# Problem 5: Smallest multiple
+# https://projecteuler.net/problem=5
+
+# 2520 is the smallest number that can be divided by each of the numbers from 1
+# to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the
+# numbers from 1 to 20?
+
+x = lcm(collect(1:20))
+
+println(x)
+
+# Answer: 232792560
+# Completed on Wed, 5 Jan 2022
---
+++ python/p005.py
@@ -0,0 +1,41 @@
+# Problem 5: Smallest multiple
+# https://projecteuler.net/problem=5
+
+# 2520 is the smallest number that can be divided by each of the numbers from 1
+# to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the
+# numbers from 1 to 20?
+
+def prime_factorization(n):
+    """Simple function that returns a dict of prime factors."""
+    i = 2
+    factors = []
+    while i <= n:
+        if n % i == 0:
+            factors.append(i)
+            n /= i
+        else:
+            i += 1
+
+    return {p:factors.count(p) for p in factors}
+
+
+# The LCM is the product of all prime factors in the range 1 to 20, with each
+#   prime raised to the highest power that appears in a factorization in this
+#   range
+factor_maps = [prime_factorization(n) for n in range(2, 21)]
+prime_counts = {p:1 for p in [2, 3, 5, 7, 11, 13, 17, 19]}
+
+for factorization in factor_maps:
+    for p, count in factorization.items():
+        prime_counts[p] = max(count, prime_counts[p])
+
+x = 1
+for k, v in prime_counts.items():
+    x *= k**v
+
+print(x)
+
+# Answer: 232792560
+# Completed on Wed, 5 Jan 2022
