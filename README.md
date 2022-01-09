49483b29d9e2d2d2f46411212a4e9a36dbce5e78
eric <emtordo@gmail.com>
2022-01-09 18:12:56 -0500

    Problem 25 solution

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ My solutions to Project Euler problems, completed as a way for me to both brush 
 | 10    | Summation of primes                   | 7 Jan 2022    |
 | 12    | Highly divisble triangular number     | 7 Jan 2022    |
 | 14    | Longest Collatz sequence              | 8 Jan 2022    |
+| 25    | 1000-digit Fibonacci number           | 9 Jan 2022    |
---
+++ python/p025.py
@@ -0,0 +1,46 @@
+# Problem 25: 1000-digit Fibonacci number
+# https://projecteuler.net/problem=25
+#
+# The Fibonacci sequence is defined by the recurrence relation:
+#
+#       Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+#
+# Hence the first 12 terms will be:
+#
+#       F1 = 1
+#       F2 = 1
+#       F3 = 2
+#       F4 = 3
+#       F5 = 5
+#       F6 = 8
+#       F7 = 13
+#       F8 = 21
+#       F9 = 34
+#       F10 = 55
+#       F11 = 89
+#       F12 = 144
+#
+# The 12th term, F12, is the first term to contain three digits.
+#
+# What is the index of the first term in the Fibonacci sequence to contain
+# 1000 digits?
+
+from math import log10
+
+
+def number_of_digits(n):
+    """Returns the number of digits in an integer n."""
+    return int(log10(n)) + 1
+
+
+i = 2
+N = 1000
+f1, f2 = 1, 1
+while number_of_digits(f2) < N:
+    f1, f2 = f2, f1 + f2
+    i += 1
+
+print(i)
+
+# Answer: 4782
+# Completed on Sun, 9 Jan 2022
