4132b0a74454f268399a42abd75c6745105feb88
eric <emtordo@gmail.com>
2022-01-12 11:01:25 -0500

    Problem 24 solution

---
+++ julia/p024.jl
@@ -0,0 +1,29 @@
+# Problem 24: Lexicographic permutations
+# https://projecteuler.net/problem=24
+#
+# A permutation is an ordered arrangement of objects. For example, 3124 is one
+# possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
+# are listed numerically or alphabetically, we call it lexicographic order.
+# The lexicographic permutations of 0, 1 and 2 are:
+#
+#       012   021   102   120   201   210
+#
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4,
+# 5, 6, 7, 8 and 9?
+
+using Combinatorics
+
+let millionth = ""
+    N = 1000000
+    digits = collect(0:9)
+    millionth_perm = Combinatorics.nthperm(digits, N)
+
+    for d in millionth_perm
+        millionth *= string(d)
+    end
+    
+    println(millionth)
+end
+
+# Answer: 2783915460
+# Completed on Wed, 12 Jan 2022
---
+++ python/p024.py
@@ -0,0 +1,28 @@
+# Problem 24: Lexicographic permutations
+# https://projecteuler.net/problem=24
+#
+# A permutation is an ordered arrangement of objects. For example, 3124 is one
+# possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
+# are listed numerically or alphabetically, we call it lexicographic order.
+# The lexicographic permutations of 0, 1 and 2 are:
+#
+#       012   021   102   120   201   210
+#
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4,
+# 5, 6, 7, 8 and 9?
+
+from itertools import permutations
+
+
+digits = list(range(10))
+perm = sorted(permutations(digits))
+
+N = 1000000
+millionth = ""
+for d in perm[N - 1]:
+    millionth += str(d)
+
+print(millionth)
+
+# Answer: 2783915460
+# Completed on Wed, 12 Jan 2022
